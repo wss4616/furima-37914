f4843c376dd35aecde46d489dc505c49146e0ef5
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-17 11:13:17 +0900

    デバイス　ストロングパラメータの記述
    nicknameの記述漏れ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :family_name_kana, :first_name, :first_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :family_name_kana, :first_name, :first_name_kana, :birthday])
   end
 
 end
