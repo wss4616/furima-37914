f47d6deaa956b43d4ab24bde39816c29a59472ae
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-17 11:13:31 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #has_many :items, dependent: :destroy
+  #has_many :purchase_records,dependent: :destroy
+
+  validates :nickname,         presence: true
+  validates :family_name,      presence: true
+  validates :first_name,       presence: true
+  validates :birthday,         presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana,  presence: true
+  
 end
