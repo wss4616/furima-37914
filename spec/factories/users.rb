191ce99cb808347ec2341f7248fbb85eeb497091
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-18 14:54:06 +0900

    テストコードの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+#spec/factories/users.rb
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {"1a" + Faker::Internet.password(min_length: 4)}
+    password_confirmation {password}
+    family_name           {"山田"}
+    first_name            {"太郎"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birthday              {"1930-01-31"}
+  end
+end
