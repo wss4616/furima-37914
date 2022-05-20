36dd4263b085bfa9148ec51f0730d3652560803f
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-20 21:29:17 +0900

    商品の出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-  has_one_attached :image
+  before_action :authenticate_user!, except: [:index, :show]
+  # has_one_attached :image
 
-  def index
+  def index 
+    @items = Item.order("created_at DESC")
   end
 
   def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
@@ -18,6 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :brand_id, :status_id, :delivery_charge_bearer_id, :prefecture_id, :delivery_day_id, :image, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,36 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  belongs_to :burand
+  has_one_attached :image
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :brand
   belongs_to :delivery_charge_bearer
   belongs_to :delivery_day
   belongs_to :prefecture
   belongs_to :status
 
-  validates :name, :text, presence: true
-  validates :brand_id,                     numericality: { other_than: 1 , message: "can't be blank"} }
-  validates :delivery_charge_bearer,       numericality: { other_than: 1 , message: "can't be blank"} }
-  validates :prefecture_id,                numericality: { other_than: 1 , message: "can't be blank"} }
-  validates :delivery_day,                 numericality: { other_than: 1 , message: "can't be blank"} }
-  validates :status_id,                    numericality: { other_than: 1 , message: "can't be blank"} }
+  with_options presence: true do
+  validates :name
+  validates :image
+  validates :brand_id
+  validates :delivery_charge_bearer_id
+  validates :prefecture_id
+  validates :delivery_day_id
+  validates :status_id
+  validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+   validates :brand_id
+   validates :delivery_charge_bearer_id
+   validates :prefecture_id
+   validates :delivery_day_id
+   validates :status_id
+  end
+
+  validates_inclusion_of :price, in: 300..9999999
+  validates :price, numericality: { with: /\A[0-9]+\z/}
+
 end
