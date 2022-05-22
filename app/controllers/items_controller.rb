850f9434568015b1e2492097d5035b89cdda7b34
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-22 13:21:25 +0900

    商品詳細遷移時の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index 
     @items = Item.order("created_at DESC")
@@ -18,12 +17,20 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
 
-
+  
 
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :brand_id, :status_id, :delivery_charge_bearer_id, :prefecture_id, :delivery_day_id, :image, :price).merge(user_id: current_user.id)
+  end
+
+  def set_action
+    @item = Item.find(params[:id])
   end
 end
