c19bfa51c35daf629f33876ec844325a36bbe1b8
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-05-26 16:48:47 +0900

    Formオブジェクトの設定とクレジットカード機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
-def index
-  @order_address = OrderAddress.new
-end
+ def index
+ @order_address = OrderAddress.new
 
-def create
-end
+ end
 
+ def create
+     @order_address = OrderAddress.new(order_params)
+     if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+ end
+
+private
+
+
+  def set_item
+   @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+   params.require(:order_address).permit(:post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,17 +3,16 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :post_number, :prefecture_id,	:city, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
-    validates :post_number, presence: { message: 'は必須項目です' },format: { with: /\A\d{3}-\d{4}\z/, allow_blank: true, message: 'は000-0000の形でのみ登録可能です' }
-    validates :prefecture_id, presence:  { message: 'は必須項目です' }
-    validates :city, presence:  { message: 'は必須項目です' }
-    validates :phone_number, numericality: { only_integer: true, message: 'は必須項目です' }
+    validates :post_number, format: { with: /\A\d{3}-\d{4}\z/}
+    validates :city
+    validates :house_number
+    validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input only number"}
     validates :user_id
     validates :item_id
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :token
   end
-
-  with_options numericality: { other_than: 1 } do
-    validates :prefecture_id
-  end
+  
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
